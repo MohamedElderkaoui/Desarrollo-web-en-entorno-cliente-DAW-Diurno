2825d19648b086dc12fc9385e26d63d75a0d55e3
mohamed <mderkaoui10@gmail.com>
2022-11-11 10:28:18 +0100

    taller js 1

---
+++ adivina-numero-m/Untitled-2.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+// Path: adivina-numero\Untitled-3.js
+const numINTENTOS = 3;
+console.log("va "+numINTENTOS+" intentos");
---
+++ adivina-numero-m/test.js
@@ -0,0 +1,5 @@
+console.log("Hello World")
+// Path: adivina-numero\Untitled-3.js
+const numINTENTOS = 3
+console.log("va "+numINTENTOS+" intentos")
+
