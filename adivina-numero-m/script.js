79c2b8b23d7b3d858a9eef251d3952799aeaa4ce
mohamed <mderkaoui10@gmail.com>
2022-11-17 12:52:50 +0100

    a12:50 2022-11-17a

--- adivina-numero-m/script.js
+++ adivina-numero-m/script.js
@@ -1,1 +1,21 @@
-'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const guessField = document.querySelector('.guess')
+const messageField = document.querySelector('.message')
+const numberField = document.querySelector('.number')
+const highscoreField = document.querySelector('.highscore')
+const scoreField = document.querySelector('.score')
+const body = document.querySelector('body')
+
+function displayMessage(msg) {
+  messageField.textContent = msg
+}
+document.querySelector('.check').addEventListener('click', () => {
+    console.log('numeric', messageField.value, guessField.value , secretNumber, score, highscore)
+    const guess = Number(guessField.value) 
+    if (!guess) displayMessage('No number!')
+}
+)
