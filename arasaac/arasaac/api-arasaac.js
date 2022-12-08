647ed1b477438275c038e61efd082aaae19a0e02
mohamed <mderkaoui10@gmail.com>
2022-12-08 21:27:04 +0100

    21:26 2022-12-08

--- arasaac/arasaac/api-arasaac.js
+++ arasaac/arasaac/api-arasaac.js
@@ -1,19 +1,40 @@
-import axios from 'axios'
-export const pedirPictos = (searchText) => {
+import axios from 'axios';
+
+export const pedirPictos = async (searchText) => {
   /* solicitar datos a api.arasaac.org */
   // 1. saber como usar axios
   // 2. url de la api
-
   const endPoint = `https://api.arasaac.org/api/pictograms/es/search/${searchText}`;
-};
+  const url = `https://api.arasaac.org/pictograms/${id}/${id}_500.png`;
 
-/*getUser*/
-async function getUser() {
-    try {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/users/1');
-        console.log(res.data);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const { data: pictos } = await axios.get(endPoint);
+    console.log(pictos);
+    console.log(pictos.id);
+    const imagearray = pictos.map(
+      ({ _id }) => `https://api.arasaac.org/pictograms/${_id}/${_id}_500.png`
+    );
+    console.log(imagearray);
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const cargarpKeywords = async (searchText) => {
+  /* solicitar datos a api.arasaac.org */
+  // 1. saber como usar axios
+  // 2. url de la api
+  const endPoint = `https://api.arasaac.org/api/pictograms/es/search/${searchText}`;
+  const url = `https://api.arasaac.org/pictograms/${id}/${id}_500.png`;
+
+  try {
+    const { data: pictos } = await axios.get(endPoint);
+    console.log(pictos);
+    console.log(pictos.id);
+    const imagearray = pictos.map(
+      ({ _id }) => `https://api.arasaac.org/pictograms/${_id}/${_id}_500.png`
+    );
+    console.log(imagearray);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- arasaac/arasaac/map.js
+++ arasaac/arasaac/map.js
@@ -24,5 +24,6 @@ export const cargarMapa = () => {
     })
     .catch((e) => {
       // do something
+      
     });
 };
